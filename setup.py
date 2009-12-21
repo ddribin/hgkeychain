377580a0e25186e6fb5ee8ae368d8703ead3082d
Jonathan Wight <jwight@mac.com>
2009-12-21 11:21:08 -0500

    Building release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
 	name = "hgkeychain",
-	version = '0.1.6',
+	version = '0.1.7',
 	author = 'Jonathan Wight',
 	author_email = 'jwight@mac.com',
 	url = 'http://toxicsoftware.com',
